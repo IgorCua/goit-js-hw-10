12df1dbe333a42fdeeeeba8e52810c378c95af0d
IhorC <igorchua@gmail.com>
2023-03-08 14:37:42 +0200

    warnings added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import {fetchCountries} from './js/fetchCountries.js'
-// let Handlebars = require("handlebars");
-var debounce = require('lodash.debounce');
 
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 const template = Handlebars.templates;
 
-input.addEventListener('input', debounce(inputHandler, 350));
-// fetchCountries('sw');
+input.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
 
 function inputHandler(event){
     const inputVal = event.target.value;
+
+    if (inputVal.length === 0){
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+        return;
+    }
+    
     fetchCountries(`${inputVal.toLowerCase()}`.trim())
     .then((data)=>{
-        console.log(data)
-        if(data.length > 1 && data.length <= 10){
+        if (data.length > 10) {
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+        } else if(data.length > 1 && data.length <= 10){
             markupArr(data)
         } else if (data.length === 1){
             markupCountry(data);
@@ -26,15 +32,17 @@ function inputHandler(event){
     })
     .catch(error => {
         console.log('error', error);
-        document.querySelector('.country-list').innerHTML = '';
-        Notiflix.Notify.failure("Oops, there is no country with that name");
+        
+        if(inputVal.length > 0){
+            Notiflix.Notify.failure("Oops, there is no country with that name");
+        }
     });
 }
 
 function markupArr(data){
     countryInfo.innerHTML = '';
     let arr = data;
-    console.log('markup arr', arr);
+    // console.log('markup arr', arr);
     countryList.innerHTML = template.array({arr});
 }
 
@@ -43,7 +51,3 @@ function markupCountry(data){
     const obj = data[0];
     countryInfo.innerHTML = template.country({obj});
 }
-// Notiflix.Notify.success('Sol lucet omnibus');
-// const template = Handlebars.templates.example;
-// document.querySelector('.country-info').innerHTML = template({doesWhat: 'rocks!'})
-//   console.log(window.Handlebars)
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 export function fetchCountries(name){
-    return fetch(`https://restcountries.com/v2/name/${name}?fullText=false`)
+    return fetch(`https://restcountries.com/v2/name/${name}?fullText=false&fields=flag,name,capital,population,languages`)
     .then((response) => {
         if(!response.ok){
             throw new Error(response.status);
